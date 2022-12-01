f9a35eef3ac710f7166a51b21380ae5805cee502
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-12-01 23:38:32 +0300

    completed the lab

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_28_095652) do
+ActiveRecord::Schema.define(version: 2022_12_23_202052) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
